782d7c091b191e2050d88553c48bb6e6595cd270
HyeongjinKim98 <201721157@sangmyung.kr>
2021-03-24 10:47:25 +0900

    implemented interface

--- interfacePrograms/src/main/java/com/example/interfaceprograms/MyInterfaceProg.java
+++ interfacePrograms/src/main/java/com/example/interfaceprograms/MyInterfaceProg.java
@@ -5,30 +5,23 @@ public class MyInterfaceProg{
         BankAccount[] accounts = new BankAccount[2];
         accounts[0] = new BankAccount(0);
         accounts[1] = new BankAccount(10000);
-        System.out.println("Average balance : "+ average(accounts));
+        System.out.println("Average Balance : "+ average(accounts));
 
         Country[] countries = new Country[2];
         countries[0] = new Country("Uruguay", 176220);
         countries[1] = new Country("Thailand",514000);
 
-        System.out.println("average area : " + average(countries));
+        System.out.println("Average Area : " + average(countries));
     }
 
-    private static double average(Country[] objects) {
+
+    private static double average(Measurable[] objects) {
         if(objects.length ==0){return 0;}
         double sum = 0;
-        for(Country obj:objects){
-            sum = sum + obj.getArea();
+        for(Measurable obj:objects){
+            sum = sum + obj.getMeasure();
         }
         return sum/objects.length;
     }
 
-    private static double average(BankAccount[] objects) {
-        if(objects.length ==0){return 0;}
-        double sum = 0;
-        for(BankAccount obj:objects){
-            sum = sum + obj.getBalance();
-        }
-        return sum/objects.length;
-    }
 }
